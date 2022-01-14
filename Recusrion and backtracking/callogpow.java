a7933b4e12278cc18931baccb0fdb516374c2eff
Ankit12bhatt <ankitbhatt640@gmail.com>
2022-01-14 23:11:06 +0530

    first commit

--- Recusrion and backtracking/callogpow.java
+++ Recusrion and backtracking/callogpow.java
@@ -1,5 +1,25 @@
+import java.util.*;
 public class callogpow {
     public static void main(String[] args) {
-        
+        Scanner in=new Scanner(System.in);
+        int x=in.nextInt();
+        int n=in.nextInt();
+        int result =pow(x, n);
+        System.out.println(result);
     }
+    public static int pow(int x, int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    int xpbn2= pow(x, n/2);
+    int result= xpbn2 * xpbn2;
+    if(n%2 == 1)
+    {
+        result= result *x;
+    }
+    return result;
+}
+
 }
