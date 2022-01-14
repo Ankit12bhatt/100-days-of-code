5f485c433c5fbc56356835247edc8073dff5c932
Ankit12bhatt <ankitbhatt640@gmail.com>
2022-01-14 21:30:19 +0530

    new files

--- Recusrion and backtracking/power.java
+++ Recusrion and backtracking/power.java
@@ -1,20 +1,19 @@
 import java.util.*;
-public class factorial {
+public class power {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        System.out.println("Enter the number");
+        int x=in.nextInt();
         int n=in.nextInt();
-        int result= fact(n);
+        int result =pow(x, n);
         System.out.println(result);
     }
-    public static int fact(int n)
+    public static int pow(int x, int n)
     {
         if(n==0)
         {
             return 1;
         }
-        int result=n*fact(n-1);
-         
+        int result= x*pow(x, (n-1));
         return result;
 
     }
